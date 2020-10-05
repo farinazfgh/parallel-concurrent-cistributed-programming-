078c2ffcc8843b4628b63af621c521472ea6875a
Farinaz Ghasemi <farinaz.ghasemi@gmail.com>
2020-10-05 16:31:11 +0200

    maven project setup

---
+++ parallel-programming-in-java/src/main/java/edu/coursera/parallel/Setup.java
@@ -0,0 +1,13 @@
+package edu.coursera.parallel;
+
+/**
+ * Hello world!
+ *
+ */
+public class Setup
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ parallel-programming-in-java/src/test/java/edu/coursera/parallel/SetupTest.java
@@ -0,0 +1,20 @@
+package edu.coursera.parallel;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple Setup.
+ */
+public class SetupTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
